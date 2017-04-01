b88f35d5d744a61d1ae4a592139be4922a22f3a9
moroine <moroine.bentefrit@gmail.com>
2017-04-01 18:25:49 +0200

    Fix dependency on un-existant service "adactive_sas_saml2_bridge.processor.hosted_idp"

--- src/Tests/Functional/Bundle/AcmeBundle/AcmeBundle.php
+++ src/Tests/Functional/Bundle/AcmeBundle/AcmeBundle.php
@@ -3,9 +3,13 @@
 namespace AdactiveSas\Saml2BridgeBundle\Tests\Functional\Bundle\AcmeBundle;
 
 
+use AdactiveSas\Saml2BridgeBundle\Tests\Functional\Bundle\AcmeBundle\DependencyInjection\AcmeBundleExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AcmeBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new AcmeBundleExtension();
+    }
 }
